24a767678d804dc9f3b11368332a52762edbebdb
Tim <timbim8996@gmail.com>
2019-12-09 10:57:54 -0800

    pushing code with new additions TV 12/9/19

--- FileWriter/src/app/App.java
+++ FileWriter/src/app/App.java
@@ -4,6 +4,8 @@ import java.io.FileWriter;
 import java.lang.String;
 import java.util.Formatter;
 import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,14 +13,29 @@ public class App {
         FileWriter fw = new FileWriter("File.txt");
 
         String str = new String("Hello World.xxxxxxxxx");
-     // String str = new String.format("Hello World, %6.3d", 123.456);
+        //String str = new String.format("Hello World, %6.3d", 123.456);
         String str2 = new String("            ");
 
        str2 = str.format("Hello world %6.3f, %d, %s, %6.1f",123.456,23,"another string",123.456);
     
-        fw.write(str2);
+       fw.write(str2);
 
-        fw.close();
+       fw.close();
+
+
+       
+
+       BufferedWriter bw = new BufferedWriter(fw);
+       PrintWriter pw = new PrintWriter(bw);
+
+       StringBuilder sb = new StringBuilder();
+
+       sb.append("Hello World \n");
+       sb.append(123.456 + "\n");
+       sb.append(17);
+
+       pw.write(sb.toString());
+       pw.close();
 
     }
 }
